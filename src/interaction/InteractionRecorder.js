48f32fa9da2beb8ce2784faabc4f30444c534c57
Luca Goettle <lgoettle@stud.hs-heilbronn.de>
2021-07-08 16:39:51 +0200

    added isValid attr

--- src/interaction/InteractionRecorder.js
+++ src/interaction/InteractionRecorder.js
@@ -8,15 +8,15 @@ const InteractionRecorder = class InteractionRecorder {
     this.interactions = [];
   }
 
-  recordInteraction({posX, posY, target}) {
+  recordInteraction({posX, posY, target, isValid = false}) {
     const timestamp = this.timer.getPassedTime();
-    this.interactions.push({timestamp, posX, posY, target});
+    this.interactions.push({timestamp, posX, posY, target, isValid});
   }
   getAllInteractions() {
     return Object.assign([], this.interactions);
   }
   getValidInteractions() {
-    return Object.assign([], this.interactions);
+    return Object.assign([], this.interactions.filter(i => i && i.isValid));
   }
 };
 
